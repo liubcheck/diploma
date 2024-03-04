9df4b76a46e7a1be0a77a45a89510bd4db684ab5
liubcheck <lyubomirvs@gmail.com>
2024-03-04 12:31:46 +0100

    Initial commit

---
+++ eureka-server/src/test/java/liubomyr/stepanenko/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package liubomyr.stepanenko.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
