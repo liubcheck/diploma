9b167cf0dc7ad5917aab35542db413b5fd8a425c
liubcheck <lyubomirvs@gmail.com>
2024-03-14 12:57:03 +0100

    Deployment updates

--- api-gateway/src/main/java/liubomyr/stepanenko/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/liubomyr/stepanenko/apigateway/ApiGatewayApplication.java
@@ -3,9 +3,6 @@ package liubomyr.stepanenko.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,13 +11,4 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
-
-    @Bean
-    public RouteLocator routes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("variants", r -> r.path("/api/variants/**")
-                        .uri("lb://lesson-service"))
-                .build();
-    }
-
 }
